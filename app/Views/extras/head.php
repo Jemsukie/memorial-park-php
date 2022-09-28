3df569a128e4bbe3fb45996ef00d58b69af1952b
Jemuel Lupo <jemuel.lupo@gmail.com>
2022-09-28 09:58:06 +0800

    do this somewhere

--- app/Views/extras/head.php
+++ app/Views/extras/head.php
@@ -4,12 +4,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=  $title ?></title>
     <link rel="icon" href="<?= base_url('favicon.ico') ?>" type="image/png">
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>">
+    <link rel="stylesheet" href="https://memorial-park.000webhostapp.com/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@2.1.4/css/boxicons.css">
-    <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>">
+    <link rel="stylesheet" href="https://memorial-park.000webhostapp.com/style.css">
     <script src="<?= base_url('assets/js/jquery.js')?>"></script>
     <script src="<?= base_url('assets/js/bootstrap.bundle.min.js')?>"></script>
-    <script src="<?= base_url('assets/js/sidebar.js')?>"></script>
+    <script src="https://memorial-park.000webhostapp.com/sidebar.css"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
